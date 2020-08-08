2bd32dd8b85808564f921219b900fe00786bb75f
SHULK1033 <andres10333@hotmail.com>
2020-08-08 18:03:15 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Card />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Card,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">My Store</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li v-for="item in items" :key="item.route" class="nav-item">
+          <a class="nav-link" v-bind:href="item.route">{{ item.name }}</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href class="btn btn-secondary text-white">
+            <i class="fas fa-shopping-cart"></i> 0
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          route: "/",
+          name: "Home",
+        },
+        {
+          route: "/store",
+          name: "Teinda",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
